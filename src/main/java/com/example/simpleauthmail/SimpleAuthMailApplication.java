05fb5f49ae7ea193aa97e9465b4676f87f7667c7
Jean-Louis <ahouansejeanlouis@gmail.com>
2023-01-03 03:15:22 +0100

    Project bootstrapping

---
+++ src/main/java/com/example/simpleauthmail/SimpleAuthMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleauthmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAuthMailApplication.class, args);
+    }
+
+}
